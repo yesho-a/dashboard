dbee99dce11381728f2fe71fb1f5edc38d7308c6
Yesho Simon <sepkap123@gmail.com>
2022-05-11 12:35:28 +0300

    removed general elements

--- sidebar.php
+++ sidebar.php
@@ -137,18 +137,6 @@
                   <p>Advanced Elements</p>
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="pages/forms/editors.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Editors</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/forms/validation.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Validation</p>
-                </a>
-              </li>
             </ul>
           </li>
           <li class="nav-item">
