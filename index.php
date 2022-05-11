050728027055a12ce9efde75dcfc7b12c91094dd
Yesho Simon <sepkap123@gmail.com>
2022-05-11 12:52:45 +0300

    added footer

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php include('header.php')?>
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
-
 <?php include('navbar.php')?>
 <?php include('sidebar.php')?>
 <?php include('footer.php')?>
