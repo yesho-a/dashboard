147e0ba6d4ed0573094177eb5442e47e4657ffcf
Yesho Simon <sepkap123@gmail.com>
2022-05-11 13:46:37 +0300

    remove dark mode

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php include('header.php')?>
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
 <?php include('navbar.php')?>
 <?php include('sidebar.php')?>
+<?php include('content.php')?>
 <?php include('footer.php')?>
   <!-- /wrapper -->
 </div>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="main-header navbar navbar-expand navbar-light">
+<nav class="main-header navbar navbar-expand navbar-dark">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
@@ -122,13 +122,8 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
+          <i class=""></i>
         </a>
       </li>
     </ul>
