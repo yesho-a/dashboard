9b6a23a0d5beb726eb434aebb2b6c6f2685ce555
Yesho Simon <sepkap123@gmail.com>
2022-05-11 09:57:14 +0300

    customize admin lte

--- header.php
+++ header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>This is a Simple Bootstrap 5 based Dashboard</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <script src="js/bootstrap.js"></script>
-    <script src="js/popper.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard 2</title>
 
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="css/fontawesome.min.css">
+  <!-- overlayScrollbars -->
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/adminlte.css">
 </head>
+
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php include('header.php')?>
-<body>
-<?php include('navbar.php')?>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
 
+<?php include('navbar.php')?>
+<?php include('sidebar.php')?>
 <?php include('footer.php')?>
+  <!-- /wrapper -->
+</div>
 </body>
 </html>
